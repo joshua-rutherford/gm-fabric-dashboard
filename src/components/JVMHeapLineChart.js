e0b69dfabcec8a15d0877b0b4d45436c611844a3
Shanberg <stuart.hanberg@deciphernow.com:sy>
2017-05-11 15:16:30 -0400

    Better chart style

--- src/components/JVMHeapLineChart.js
+++ src/components/JVMHeapLineChart.js
@@ -19,6 +19,31 @@ export default function JVMHeapLineChart({ maxHeap, usedAndCommittedHeapArr }) {
     usedHeap: Math.round(obj.used / BYTES_TO_MB)
   }));
 
+
+  const CustomizedYAxisTick = React.createClass({
+    render () {
+      const {x, y, stroke, payload} = this.props;
+
+     	return (
+      	<g transform={`translate(${x},${y})`}>
+          <text x={-4} y={0} dy={0} textAnchor="end" fontSize="12" fontWeight="500" fill="#666">{payload.value} MB</text>
+        </g>
+        );
+      }
+    });
+
+    const CustomizedXAxisTick = React.createClass({
+      render () {
+        const {x, y, stroke, payload} = this.props;
+
+       	return (
+        	<g transform={`translate(${x},${y})`}>
+            <text x={0} y={16} dy={0} textAnchor="middle" fontSize="12" fontWeight="500" fill="#666">{payload.value}</text>
+          </g>
+          );
+        }
+      });
+
   return (
     <div className="uk-card uk-card-small uk-card-body">
       <h3 className="uk-card-title">Heap</h3>
@@ -33,24 +58,42 @@ export default function JVMHeapLineChart({ maxHeap, usedAndCommittedHeapArr }) {
             dot={false}
             isAnimationActive={false}
             name="Committed Heap Size"
-            stroke="#ffc700"
+            stroke="#555"
+            strokeWidth="2"
             type="monotone"
+            unit=" MB"
           />
           <Line
             dataKey="usedHeap"
             dot={false}
             isAnimationActive={false}
             name="Used Heap Size"
-            stroke="#00a1ff"
+            stroke="#0aab2a"
+            strokeWidth="2"
             type="monotone"
+            unit=" MB"
           />
-          <CartesianGrid stroke="#ccc" />
+        <CartesianGrid
+          stroke="#eee"
+          strokeWidth="1"
+        />
           <XAxis
             dataKey="prettyTime"
             interval="preserveStartEnd"
-            padding={{ left: 25 }}
+            tickSize="1"
+            minTickGap="16"
+            tick={<CustomizedXAxisTick/>}
+            axisLine={false}
+            tickLine={false}
           />
-          <YAxis allowDecimals={false} />
+          <YAxis allowDecimals={false}
+            mirrored="true"
+            tickSize="1"
+            tick={<CustomizedYAxisTick/>}
+            minTickGap="16"
+            axisLine={false}
+            tickLine={false}
+           />
           <Tooltip />
         </LineChart>
       </ResponsiveContainer>
