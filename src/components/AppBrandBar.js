49cbd602ae601691fa240c5985979682985ff6aa
Sean McBride <spmcbride1201@gmail.com>
2017-07-28 12:16:31 -0400

    chore: Rev App version

--- src/components/AppBrandBar.js
+++ src/components/AppBrandBar.js
@@ -1,21 +1,19 @@
 import React from "react";
 import decipherLogo from "../images/decipherLogo.svg";
 
-const AppVersion="0.1.1"
+const AppVersion = "0.2.0";
 
-const BrandName="Fabric"
-const BrandLogo=decipherLogo
+const BrandName = "Fabric";
+const BrandLogo = decipherLogo;
 
 const AppBrandBar = () => {
   return (
     <nav className="app-header app-brandbar">
       <div className="brand">
-        <img
-          alt={BrandName + 'Logo'}
-          className="brand-logo"
-          src={BrandLogo}
-        />
-        <span className="brand-text" id="logo-text">{BrandName}</span>
+        <img alt={BrandName + "Logo"} className="brand-logo" src={BrandLogo} />
+        <span className="brand-text" id="logo-text">
+          {BrandName}
+        </span>
       </div>
 
       <a
@@ -26,7 +24,6 @@ const AppBrandBar = () => {
       >
         {AppVersion}
       </a>
-
     </nav>
   );
 };
